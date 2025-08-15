a02f8705bd7d82466f8bca98dc3ba7ff687b77dc
Hussain Shauzab Abis <hussain0305@users.noreply.github.com>
2025-08-15 15:10:20 +0300

    HUD Widgets Implemented

--- Source/BurstRhythmGame/Private/ThirdPartyDecoders.cpp
+++ Source/BurstRhythmGame/Private/ThirdPartyDecoders.cpp
@@ -1,5 +1,5 @@
 #define DR_WAV_IMPLEMENTATION
-#include "../ThirdParty\AudioDecoders/dr_wav.h"
+#include "../ThirdParty/AudioDecoders/dr_wav.h"
 
 #define MINIMP3_IMPLEMENTATION
-#include "../ThirdParty\AudioDecoders/minimp3_ex.h"
+#include "../ThirdParty/AudioDecoders/minimp3_ex.h"
